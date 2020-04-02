cffdebef5f1c2afe90ad09d5ea163c305d3c0581
예림 <@192.168.120.30>
2020-04-02 17:40:09 +0900

    하는중입니다

--- src/main/java/com/kh/brocoli/board/controller/BoardController.java
+++ src/main/java/com/kh/brocoli/board/controller/BoardController.java
@@ -1,5 +1,13 @@
 package com.kh.brocoli.board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/board")
 public class BoardController {
+
+	
 
 }
