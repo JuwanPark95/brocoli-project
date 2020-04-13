5c118f38caf6dd844384346d503dac2959f9b286
Juwan_p <rochpjw@gmail.com>
2020-04-13 15:11:19 +0900

    오너 상품정보변경 제작중..

--- src/main/java/com/kh/brocoli/owner/model/service/ownerProductServiceimpl.java
+++ src/main/java/com/kh/brocoli/owner/model/service/ownerProductServiceimpl.java
@@ -63,6 +63,21 @@ public class ownerProductServiceimpl implements ownerProductService {
 		return oDao.stockList(bNO);
 	}
 
+	@Override
+	public Product productSet(int PNO) {
+		return oDao.productSet(PNO);
+	}
+
+	@Override
+	public Product_File productFileSet(int PNO) {
+		return oDao.productFilrSet(PNO);
+	}
+
+	@Override
+	public ArrayList<Product_Option> prodictOption(int PNO) {
+		return oDao.productOptionset(PNO);
+	}
+
 
 
 }
