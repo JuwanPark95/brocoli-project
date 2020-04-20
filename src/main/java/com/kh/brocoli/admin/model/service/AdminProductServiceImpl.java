a1ee6f56853b17d2c0093a26f48dfe022fdc0336
Eunji <Eunji@DESKTOP-K5NMK49>
2020-04-20 13:32:49 +0900

    admin 메퍼 ,,,히히

---
+++ src/main/java/com/kh/brocoli/admin/model/service/AdminProductServiceImpl.java
@@ -0,0 +1,44 @@
+package com.kh.brocoli.admin.model.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.brocoli.admin.model.dao.AdminProductDao;
+import com.kh.brocoli.product.model.vo.Product;
+import com.kh.brocoli.product.model.vo.Product_Option;
+
+@Service("APSerivce")
+public class AdminProductServiceImpl implements AdminProductService {
+
+	@Autowired
+	private AdminProductDao APDao;
+
+	/**
+	 * 작성자 : 신은지
+	 * 1. 상품 list
+	 */
+	@Override
+	public ArrayList<Product> selectProductList() {
+		return APDao.selectProductList();
+	}
+
+	/**
+	 * 작성자 : 신은지
+	 * 2. 상품 상세보기
+	 */
+	@Override
+	public ArrayList<Product> selectproductDetail(int p_NO) {
+		return APDao.selectproductDetail(p_NO);
+	}
+
+	/**
+	 * 작성자 : 신은지
+	 * 3. 상품 재고 list
+	 */
+	@Override
+	public ArrayList<Product> selectStockManagement() {
+		return APDao.selectStockManagement();
+	}
+}
