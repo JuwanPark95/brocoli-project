0cb321e51a74dc9abd2c1ea3411ad9e47381271a
예림 <@192.168.1.142>
2020-04-23 19:21:57 +0900

    장바구니 리스트 뽑는디 수량이랑 총금액 삭제 기능 남음

--- src/main/java/com/kh/brocoli/member/model/service/CartService.java
+++ src/main/java/com/kh/brocoli/member/model/service/CartService.java
@@ -1,10 +1,14 @@
 package com.kh.brocoli.member.model.service;
 
+import java.util.ArrayList;
+
 import com.kh.brocoli.member.model.vo.Cart;
 import com.kh.brocoli.product.model.vo.ProductDetail;
 
 public interface CartService {
 
 	public int cInsert(Cart c);
+
+	public ArrayList<Cart> cartList(int ct_Mno);
 
 }
