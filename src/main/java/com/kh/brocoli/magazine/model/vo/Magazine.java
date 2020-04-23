c13ca8fb27379cd4fc57c5ce49d31cf241838913
Eunji <Eunji@DESKTOP-K5NMK49>
2020-04-23 11:18:58 +0900

    amdin magazine mapper vo 수정

--- src/main/java/com/kh/brocoli/magazine/model/vo/Magazine.java
+++ src/main/java/com/kh/brocoli/magazine/model/vo/Magazine.java
@@ -13,25 +13,33 @@ public class Magazine {
 	 private String m_Count;
 	 private int m_Brand_NO;
 	 private int m_Mno;
-	  
+	 
+	 private String m_Magazine_File;  // admin 매거진파일
+	 private String m_Brand;		  // admin 매거진브랜드 
+	 private String m_Member;         // admin 매거진멤버
+	
 	 public Magazine() {
-     	  super();
+		super();
 	 }
 
-     public Magazine(int m_NO, String m_ID, String m_Title, String m_Comment, Date m_Date, String m_Status,
-		  String m_Category, String m_Count, int m_Brand_NO, int m_Mno) {
-	  super();
-	  this.m_NO = m_NO;
-	  this.m_ID = m_ID;
-	  this.m_Title = m_Title;
-	  this.m_Comment = m_Comment;
-	  this.m_Date = m_Date;
-	  this.m_Status = m_Status;
-	  this.m_Category = m_Category;
-	  this.m_Count = m_Count;
-	  this.m_Brand_NO = m_Brand_NO;
-	  this.m_Mno = m_Mno;
-    }
+	public Magazine(int m_NO, String m_ID, String m_Title, String m_Comment, Date m_Date, String m_Status,
+			String m_Category, String m_Count, int m_Brand_NO, int m_Mno, String m_Magazine_File, String m_Brand,
+			String m_Member) {
+		super();
+		this.m_NO = m_NO;
+		this.m_ID = m_ID;
+		this.m_Title = m_Title;
+		this.m_Comment = m_Comment;
+		this.m_Date = m_Date;
+		this.m_Status = m_Status;
+		this.m_Category = m_Category;
+		this.m_Count = m_Count;
+		this.m_Brand_NO = m_Brand_NO;
+		this.m_Mno = m_Mno;
+		this.m_Magazine_File = m_Magazine_File;
+		this.m_Brand = m_Brand;
+		this.m_Member = m_Member;
+	}
 
 	public int getM_NO() {
 		return m_NO;
@@ -113,11 +121,36 @@ public class Magazine {
 		this.m_Mno = m_Mno;
 	}
 
+	public String getM_Magazine_File() {
+		return m_Magazine_File;
+	}
+
+	public void setM_Magazine_File(String m_Magazine_File) {
+		this.m_Magazine_File = m_Magazine_File;
+	}
+
+	public String getM_Brand() {
+		return m_Brand;
+	}
+
+	public void setM_Brand(String m_Brand) {
+		this.m_Brand = m_Brand;
+	}
+
+	public String getM_Member() {
+		return m_Member;
+	}
+
+	public void setM_Member(String m_Member) {
+		this.m_Member = m_Member;
+	}
+
 	@Override
 	public String toString() {
 		return "Magazine [m_NO=" + m_NO + ", m_ID=" + m_ID + ", m_Title=" + m_Title + ", m_Comment=" + m_Comment
 				+ ", m_Date=" + m_Date + ", m_Status=" + m_Status + ", m_Category=" + m_Category + ", m_Count="
-				+ m_Count + ", m_Brand_NO=" + m_Brand_NO + ", m_Mno=" + m_Mno + "]";
+				+ m_Count + ", m_Brand_NO=" + m_Brand_NO + ", m_Mno=" + m_Mno + ", m_Magazine_File=" + m_Magazine_File
+				+ ", m_Brand=" + m_Brand + ", m_Member=" + m_Member + "]";
 	}
 	  
 }
